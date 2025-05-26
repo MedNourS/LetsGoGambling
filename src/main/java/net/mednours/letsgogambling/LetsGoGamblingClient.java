a315bc07159e0e15b393cc4672d61700a3cee85c
MedNourS <sabir.med.nour@gmail.com>
2025-05-25 22:12:52 -0400

    Initial commit

---
+++ src/main/java/net/mednours/letsgogambling/LetsGoGamblingClient.java
@@ -0,0 +1,13 @@
+package net.mednours.letsgogambling;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LetsGoGamblingClient implements ClientModInitializer {
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
